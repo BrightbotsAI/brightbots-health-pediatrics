d7307759a0990e232acbec06f691c3ddf4489dbf
Sergio Alejandro Parra Ordoñez <sergioparra@MacBook-Air-de-Sergio.local>
2023-06-15 19:21:41 -0500

    Documentation

--- src/pages/GetCarePage.tsx
+++ src/pages/GetCarePage.tsx
@@ -7,17 +7,17 @@ export function GetCare(): JSX.Element {
 
   return (
     <Document width={800}>
-      <Scheduler
-        schedule={schedule as Schedule}
+      <Scheduler //This is the fuction that permit that the page show the calendary to get the care with the day and the month
+        schedule={schedule as Schedule}//the function also give us the disponibles hours for the care
         questionnaire={{
           resourceType: 'Questionnaire',
           name: 'Test',
           item: [
-            {
-              id: 'id-1',
-              linkId: 'q1',
-              type: 'string',
-              text: 'Question 1',
+            {//This are the questions optionals that we can write sickness or important things for the care
+              id: 'id-1',//first the id to identify the component
+              linkId: 'q1',//the link to linked the id
+              type: 'string',// The type of answer (text, numbers, date, etc)
+              text: 'Question 1',//the text that show the programm to orient the user
             },
             {
               id: 'id-2',
