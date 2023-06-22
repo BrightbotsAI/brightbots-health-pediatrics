8888fed6d46f55c78ed8b8045acdbeac5c5cd20a
DanielL <jdlozada@uniempresarial.edu.co>
2023-06-22 16:15:29 -0500

    ✅Update of font type, color and calendar section

--- src/pages/calendar/index.tsx
+++ src/pages/calendar/index.tsx
@@ -1,6 +1,6 @@
 import { Slot } from "@medplum/fhirtypes";
 import { Document, CalendarInput } from "@medplum/react";
-
+import { Text, Title, Center} from '@mantine/core';
 
 export function CalendarMenu(): JSX.Element {
     
@@ -8,16 +8,23 @@ export function CalendarMenu(): JSX.Element {
     const end = new Date(start);
     end.setHours(start.getHours() + 1);
     return (
+      <>
+      <br />
       <Document>
-        <CalendarInput slots={([{
-        resourceType: 'Slot',
-        schedule: {
-            reference: 'Schedule/example'
-        },
-        status: 'free',
-        start: start.toISOString(),
-        end: end.toISOString()
-        }] as Slot[])} onChangeMonth={(date: Date) => console.log(date)} onClick={(date: Date) => console.log('Clicked ' + date)} />
+        <Title align="center" weight={700} size="3.75rem" order={1}>Calendar</Title>
+        <Document>
+          <CalendarInput slots={([{
+          resourceType: 'Slot',
+          schedule: {
+              reference: 'Schedule/example'
+          },
+          status: 'free',
+          start: start.toISOString(),
+          end: end.toISOString()
+          }] as Slot[])} onChangeMonth={(date: Date) => console.log(date)} onClick={(date: Date) => console.log('Clicked ' + date)} />
+          
+        </Document>
       </Document>
+      </>
     );
   }
