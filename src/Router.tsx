119d62ab5b5043aa6190c9e992bbaa2ea1a40d7b
Sergio Alejandro Parra Ordoñez <sergioparra@MacBook-Air-de-Sergio.local>
2023-06-23 18:10:09 -0500

    ✨ Correction appoiments

--- src/Router.tsx
+++ src/Router.tsx
@@ -20,16 +20,16 @@ import { HomePage } from './pages/HomePage';
 import { Messages } from './pages/MessagesPage';
 import { ObservationPage } from './pages/ObservationPage';
 import { SignOutPage } from './pages/SignOutPage';
-import { FormularyGeneral } from './pages/Appoiments/GeneralAppoiment';
-import { FormularySpecial } from './pages/Appoiments/SpecialAppoiment';
+import { FormularyGeneral } from './pages/appoiments/GeneralAppoiment';
+import { FormularySpecial } from './pages/appoiments/SpecialAppoiment';
 
 export function Router(): JSX.Element {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="messages/*" element={<Messages />} />
-      <Route path="GeneralApoiment/*" element={<FormularyGeneral />} />
-      <Route path="SpecialApoiment/*" element={<FormularySpecial />} />
+      <Route path="general-apoiment/*" element={<FormularyGeneral />} />
+      <Route path="special-apoiment/*" element={<FormularySpecial />} />
       <Route path="health-record/*" element={<HealthRecord />}>
         <Route index element={<Navigate replace to="/health-record/lab-results" />} />
         <Route path="lab-results/*" element={<LabResults />} />
