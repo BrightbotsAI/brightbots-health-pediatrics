2bdc31cc8d276282c08e969366f7c972a32e6123
Sergio Alejandro Parra Ordoñez <sergioparra@MacBook-Air-de-Sergio.local>
2023-06-27 17:47:00 -0500

    �<009f><009a>�Cambios al Sheduler

--- src/pages/appointments/SpecialAppointment.tsx
+++ src/pages/appointments/SpecialAppointment.tsx
@@ -1,87 +1,96 @@
-import { Center, Container, TextInput, Button, Group, Box, NumberInput, NativeSelect } from "@mantine/core";
-import { useForm } from '@mantine/form';
+import { Schedule } from '@medplum/fhirtypes';
+import { Document, Scheduler, useMedplum } from '@medplum/react';
 
 export function FormularySpecial(): JSX.Element{
-  const form = useForm({
-    initialValues: {
-      name: '',
-      Type_of_ID: '',
-      ID: '',
-      Gender: '',
-      Phone: '',
-      Condition:'',
-      Calendar: ''
-    },
-    validate:{
-      name: (value) => {
-        if (!/^[A-Za-z]+$/.test(value)) {
-          return 'Name must contain only letters';
-        }
-        return null;
-      },
-      ID: (value) => {
-        if (!/^\d+$/.test(value)) {
-          return 'ID must contain only numbers';
-        }
-        return null;
-      },
-      Phone: (value) => {
-        if (!/^\d+$/.test(value)) {
-          return 'Phone must contain only numbers';
-        }
-        return null;
-      }
-    }
-      });
+  const medplum = useMedplum();
+  const schedule = medplum.searchOne('Schedule').read();
   return(
-    <div>
-      <h1 id="ScheduleGeneral" style={{color: "#00ABC1", fontFamily: "Roboto", fontWeight: "bold", marginLeft: 10}}>  Schedule special appointment</h1>
-      <br /><br />
-      <Container px={385} mx="auto">
-        <Center style={{borderTop: 4,border: "solid", borderColor:"#00ABC1", borderRadius: "10px"}}>
-        <Box maw={300} mx="auto">
-      <form onSubmit={form.onSubmit((values) => console.log(values))}>
-          <TextInput
-          withAsterisk
-          label="Name:"
-          {...form.getInputProps('name')}
-          style={{borderBottomWidth: 1, borderBottom: "solid", marginBottom: 4, marginTop: 6}}/>
-          <NativeSelect
-          data={["Driver's license from a U.S. state", "Federal or state ID card","Military ID card","U.S. passport"]}
-          label="Type of ID:"
-          style={{marginBottom: 4, color: "#00ABC1"}}
-          withAsterisk/>
-          <NumberInput
-          withAsterisk
-          label="ID:"
-          {...form.getInputProps('ID')}
-          style={{borderBottomWidth: 1, borderBottom: "solid", marginBottom: 4}}/>
-          <NativeSelect
-          data={["Male","Female"]}
-          label="Gender:"
-          style={{marginBottom: 4}}
-          withAsterisk/>
-          <NumberInput
-          withAsterisk
-          label="Phone:"
-          {...form.getInputProps('Phone')}
-          style={{borderBottomWidth: 1, borderBottom: "solid", marginBottom: 4}}/>
-          <TextInput
-          withAsterisk
-          label="Condition:"
-          {...form.getInputProps('Condition')}
-          style={{borderBottomWidth: 1, borderBottom: "solid", marginBottom: 4}}/>
-          <label>
-           Calendar:
-           <input type="date" style={{border: 0}}/>
-          </label>
-        <Group position="center" mt="md">
-          <Button type="submit" style={{marginBottom: 6, backgroundColor: "#00ABC1"}}>Submit</Button>
-        </Group>
-      </form>
-          </Box>
-        </Center>
-      </Container>
-    </div>
-  )
+    <Document width={800}>
+      <h1  id="ScheduleSpecial" style={{color: "#00ABC1", fontFamily: "Roboto", fontWeight: "bold"}}> Schedule general appointment </h1>
+      <Scheduler //This is the fuction that permit that the page show the calendary to get the care with the day and the month
+        schedule={schedule as Schedule}//the function also give us the disponibles hours for the care
+        questionnaire={{
+          resourceType: 'Questionnaire',
+          name: 'Test',
+          item: [
+            {//This are the questions optionals that we can write sickness or important things for the care
+              id: 'SpecialName',//first the id to identify the component
+              linkId: 'q1',//the link to linked the id
+              type: 'string',// The type of answer (text, numbers, date, etc)
+              text: 'Name:',//the text that show the programm to orient the user
+            },
+            {
+              id: 'SpecialType',
+              linkId: 'q2',
+              type: 'choice',
+              answerOption: [{
+                valueCoding:{
+                  code: "Driver's license from a U.S. state",
+                  display: "Driver's license from a U.S. state"
+                }
+              },
+              {
+                valueCoding:{
+                  code: "Federal or state ID card",
+                  display: "Federal or state ID card"
+                }
+              },
+              {
+                valueCoding:{
+                  code: "Military ID card",
+                  display: "Military ID card"
+                }
+              },
+              {
+                valueCoding:{
+                  code: "U.S. passport",
+                  display: "U.S. passport"
+                }
+              }
+            ],
+              text: 'Type of ID:',
+            },
+            {
+              id: 'SpecialID',
+              linkId: 'q3',
+              type: 'integer',
+              text: 'ID:',
+            },
+            {
+              id: 'SpecialGender',
+              linkId: 'q3',
+              type: 'choice',
+              answerOption: [{
+                valueCoding:{
+                  code: "Male",
+                  display: "Male"
+                }
+              },
+              {
+                valueCoding:{
+                  code: "Female",
+                  display: "Female"
+                }
+              }
+            ],
+              text: 'Gender:',
+            },
+            {
+              id: 'SpecialPhone',
+              linkId: 'q3',
+              type: 'integer',
+              text: 'Phone:',
+            },
+            {
+              id: 'SpecialCondition',
+              linkId: 'q3',
+              type: 'string',
+              text: 'Cpndition:',
+            }
+          ],
+        }}
+      />
+    </Document>
+  );
 }
+
