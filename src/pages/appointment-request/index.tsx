ddb17e016e73be6f9fe4a2b5ead09634f3865a24
Cristian Medina <cristianc@brightbots.co>
2023-06-29 15:07:18 -0500

    🚸 User experience navigation flow Improved

--- src/pages/appointment-request/index.tsx
+++ src/pages/appointment-request/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 export function Pagerequest(): JSX.Element {
@@ -8,11 +9,15 @@ export function Pagerequest(): JSX.Element {
       </section>
       <section className="image_request">
         <div className="general_app">
-          <img src="https://images.ecestaticos.com/Bosqgwi-bRoiiu2s89AjrlamxUE=/0x70:1716x1040/1338x751/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2F8db%2F8b6%2Faa5%2F8db8b6aa54b585253e15f79a68447aeb.jpg" />
+          <Link to="/general-appointment">
+            <img src="https://images.ecestaticos.com/Bosqgwi-bRoiiu2s89AjrlamxUE=/0x70:1716x1040/1338x751/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2F8db%2F8b6%2Faa5%2F8db8b6aa54b585253e15f79a68447aeb.jpg" />
+          </Link>
           <h2>General appointment</h2>
         </div>
         <div className="general_app">
-          <img src="https://cdn2.salud180.com/sites/default/files/check_up_medico_para_adultos_mayores_2_0.jpg" />
+          <Link to="/special-appointment/*">
+            <img src="https://cdn2.salud180.com/sites/default/files/check_up_medico_para_adultos_mayores_2_0.jpg" />
+          </Link>
           <h2>Special appointment</h2>
         </div>
       </section>
